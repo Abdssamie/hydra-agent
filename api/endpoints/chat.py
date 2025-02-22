34dcebfe2b1de3a32c6965bd62133759a2ed8233
Abdssamie <abdessamie.elmoubarki@gmail.com>
2025-02-22 19:02:56 +0100

    fixed adding system message delay

--- api/endpoints/chat.py
+++ api/endpoints/chat.py
@@ -5,12 +5,13 @@ from api.models.chat import ChatResponse, ChatResponseWithSessionID
 from agent.main import ReActAgent, memory, llm
 from agent.tools.tools import query_tool, add_numbers_tool, search_data, load_data
 from agent.prompts import DEFAULT_SYSTEM_PROMPT
-from agent.multimodal import process_image, gen_img
+from agent.multimodal import process_image  #, gen_img
 from typing import Annotated, List
 from llama_index.core.base.llms.types import ChatMessage, MessageRole
 from uuid import uuid4
 from logging_config import setup_logging
 from agent.clients import client
+import asyncio
 
 setup_logging()
 
@@ -28,6 +29,17 @@ agent = ReActAgent(
 )
 
 
+async def ensure_system_message_stored(session_key, retries=8, delay=0.01):
+    """Waits until the system message is stored, with retries."""
+    for _ in range(retries):
+        messages = memory.chat_store.get_messages(key=session_key)
+        if any(msg.role == MessageRole.SYSTEM for msg in messages):
+            return True  # System message is present, continue execution
+        await asyncio.sleep(delay)  # Wait a short time before retrying
+    logger.warning("System message was not found after multiple attempts")
+    return False
+
+
 @router.post("/chat", )
 async def chat_endpoint(message: Annotated[str, Form()], file: UploadFile | None = File(None),
                         session_key: Annotated[str | None, Header()] = None):
@@ -41,6 +53,7 @@ async def chat_endpoint(message: Annotated[str, Form()], file: UploadFile | None
         await memory.aput(
             ChatMessage(role=MessageRole.SYSTEM, content=DEFAULT_SYSTEM_PROMPT)
         )
+        await ensure_system_message_stored(session_key)
         send_session_key = True
         logger.info(f"Session id generated: {session_key}")
 
@@ -102,7 +115,7 @@ async def delete_chat_history(session_key: str):
         raise HTTPException(status_code=500, detail=f"An error occurred during chat history deletion: {e}")
 
 
-@router.get("/generate_image/")
-async def generate_image(query: str):
-    file_path = await gen_img(client, query)
-    return FileResponse(file_path)
+# @router.get("/generate_image/")
+# async def generate_image(query: str):
+#     file_path = await gen_img(client, query)
+#     return FileResponse(file_path)
